745cfaf0e4dc01cd05b400bb3dfcade7d4e99f23
LQQuest <52664868+LQQuest@users.noreply.github.com>
2020-10-27 20:22:43 +0300

    Add files via upload

---
+++ Dot.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dot : MonoBehaviour
+{
+    public int column;
+    public int row;
+    public int tardetX;
+    public int tardetY;
+
+    private Board board;
+    private GameObject otherDot;
+    private Vector2 firstTouchPosition;
+    private Vector2 finalTouchPosition;
+    public float swipeAngle = 0;
+    
+    void Start()
+    {
+        board = FindObjectOfType<Board>();
+        tardetX = (int) transform.position.x;
+        tardetY = (int) transform.position.y;
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log(firstTouchPosition);
+        
+    }
+
+    private void OnMouseUp()
+    {
+        finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+
+    void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y,
+            finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
+        
+    }
+
+    void MovePieces()
+    {
+        if (swipeAngle > -45 && swipeAngle <= 45)
+        {
+            //Right Swipe
+            otherDot = board.allDots[column + 1, row];
+            otherDot.GetComponent<Dot>().column -= 1;
+            column += 1;
+        }
+    }
+}
